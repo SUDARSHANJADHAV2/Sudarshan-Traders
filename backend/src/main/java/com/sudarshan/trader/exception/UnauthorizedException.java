a169226b0d8c25a740352263df49d377793522cf
SUDARSHAN SANTAJI JADHAV <sudarshanjadhav2@zohomail.in>
2025-11-03 00:28:58 +0530

    DISCARDED

--- backend/src/main/java/com/sudarshan/trader/exception/UnauthorizedException.java
+++
@@ -1,7 +0,0 @@
-package com.sudarshan.trader.exception;
-
-public class UnauthorizedException extends RuntimeException {
-    public UnauthorizedException(String message) {
-        super(message);
-    }
-}
